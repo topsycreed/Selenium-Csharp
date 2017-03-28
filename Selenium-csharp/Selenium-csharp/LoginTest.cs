cb7d3f9a3ac573116719b9a61b311e106ebab8ca
topsycreed <topsycreed@gmail.com>
2017-03-28 09:05:26 +0400

    Added scenario to check all links and sublinks in menu
    Added scenario to check all links and sublinks in menu, asserting that
    for every page exist h1 tag with some text.

--- Selenium-csharp/Selenium-csharp/LoginTest.cs
+++ Selenium-csharp/Selenium-csharp/LoginTest.cs
@@ -4,6 +4,9 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using Selenium_csharp.Properties;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Selenium_csharp
 {
@@ -13,12 +16,18 @@ namespace Selenium_csharp
         private IWebDriver driver;
         private WebDriverWait wait;
 
+        bool AreElementsPresent(IWebDriver driver, By locator)
+        {
+            return driver.FindElements(locator).Count > 0;
+        }
+
         [SetUp]
         public void start()
         {
             var ChromeDriverLocation = AppDomain.CurrentDomain.BaseDirectory;
 
             driver = new ChromeDriver(ChromeDriverLocation);
+            driver.Manage().Window.Maximize();
             wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
         }
 
@@ -29,7 +38,29 @@ namespace Selenium_csharp
             driver.FindElement(By.Name("username")).SendKeys("admin");
             driver.FindElement(By.Name("password")).SendKeys("p@ssw0rd");
             driver.FindElement(By.Name("login")).Click();
-            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//div[@class='header']/a[@href='http://localhost/litecart/admin/logout.php']")));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[@title='Logout']")));
+        }
+
+        [Test]
+        public void CheckAllPages()
+        {
+            Login();
+
+            if (AreElementsPresent(driver, By.XPath("//ul[@id = 'box-apps-menu']/li/a/span[@class = 'name']")))
+            {
+                //List<IWebElement> linksToClick = driver.FindElement(By.Id("DivId")).FindElements(By.TagName("a")).ToList();
+
+                ReadOnlyCollection<IWebElement> PagesToClick = driver.FindElements(By.XPath("//ul[@id = 'box-apps-menu']/li/a/span[@class = 'name']"));
+
+                int PageCount = PagesToClick.Count();
+
+                //foreach (IWebElement Element in PagesToClick)
+                for (int i = 0; i <= PageCount - 1; i++)
+                {
+                    PagesToClick[i].Click();
+                    PagesToClick = driver.FindElements(By.XPath("//ul[@id = 'box-apps-menu']/li/a/span[@class = 'name']"));
+                }
+            }
         }
 
         [TearDown]
